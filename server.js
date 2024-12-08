85c5fe5cb153a82fc23ef6b54701045bd3a79e7c
Saifeddine Rhouma <saifeddine.rhouma91@gmail.com>
2024-12-08 04:36:25 +0100

    adding root server

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(cors());
 
 // Serve static files from the React app's build directory
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Handle all other routes by serving the React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Start the server
